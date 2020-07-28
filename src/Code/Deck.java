427b18d0b114683d4dc31aa160229de69c1f132d
yetmanj999 <59924351+yetmanj999@users.noreply.github.com>
2020-07-28 11:48:51 -0400

    Created Deck.java Basic Code

---
+++ src/Code/Deck.java
@@ -0,0 +1,30 @@
+package Code;
+
+public class Deck {
+
+    public int size() {
+        // TODO - implement Deck.size
+        throw new UnsupportedOperationException();
+    }
+
+    public void shuffle() {
+        // TODO - implement Deck.shuffle
+        throw new UnsupportedOperationException();
+    }
+
+    public Card draw() {
+        // TODO - implement Deck.draw
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * 
+     * @param element1
+     * @param element2
+     */
+    private void swapElement(int element1, int element2) {
+        // TODO - implement Deck.swapElement
+        throw new UnsupportedOperationException();
+    }
+
+}

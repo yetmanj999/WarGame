0d3ed1efc4559f99a082bb61db2dce2eed35ed6d
yetmanj999 <59924351+yetmanj999@users.noreply.github.com>
2020-07-28 11:45:01 -0400

    Created War Basic Code

---
+++ src/Code/War.java
@@ -0,0 +1,25 @@
+package Code;
+
+public class War {
+
+    private Deck deck;
+    private String playerStack;
+    private Stack pcStack;
+
+    public void play() {
+        // TODO - implement War.play
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * 
+     * @param card1
+     * @param card2
+     * @param card3
+     */
+    public void drawThreeCard(int card1, int card2, int card3) {
+        // TODO - implement War.drawThreeCard
+        throw new UnsupportedOperationException();
+    }
+
+}

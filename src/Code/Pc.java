3507e4c1b3c0302137345e9bde876c593798e107
yetmanj999 <59924351+yetmanj999@users.noreply.github.com>
2020-07-28 11:46:03 -0400

    Created Pc.java Basic Code

---
+++ src/Code/Pc.java
@@ -0,0 +1,41 @@
+package Code;
+
+public class Pc {
+
+    private Stack discardStack;
+    private Stack drawStack;
+
+    public Card draw() {
+        // TODO - implement Pc.draw
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * 
+     * @param Card
+     */
+    public void discard(int Card) {
+        // TODO - implement Pc.discard
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * 
+     * @param Card
+     */
+    public void addToDrawStack(int Card) {
+        // TODO - implement Pc.addToDrawStack
+        throw new UnsupportedOperationException();
+    }
+
+    public boolean hasCard() {
+        // TODO - implement Pc.hasCard
+        throw new UnsupportedOperationException();
+    }
+
+    public String toString() {
+        // TODO - implement Pc.toString
+        throw new UnsupportedOperationException();
+    }
+
+}

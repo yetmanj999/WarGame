1a892caf1feb841674ae33869fcf89980a47ec83
yetmanj999 <59924351+yetmanj999@users.noreply.github.com>
2020-08-10 15:46:10 -0400

    Update Stack.java
    JEEJ.JO updated Stack

--- src/Code/Stack.java
+++ src/Code/Stack.java
@@ -1,8 +1,22 @@
 package Code;
 
+import java.util.List;
+
 public class Stack {
 
     private Stack playerStack;
     private Stack pcStack;
+
+    Card pop() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    int size() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void addAll(List<Card> war1) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 
 }

0d8ac1e2d15afc6b68cb39cceddc98267684af18
yetmanj999 <59924351+yetmanj999@users.noreply.github.com>
2020-08-11 20:49:13 -0400

    Update Deck.java

--- src/Code/Deck.java
+++ src/Code/Deck.java
@@ -1,5 +1,9 @@
 package Code;
-
+/*
+ * @authors: Jake Yetman, Joshua Santos, Kolade Siyanbola, Muhammad Hassan Asif
+ * Date: 8/11/2020
+ * Description: Program creates deck by extending GroupOfCards
+*/ 
 public class Deck extends GroupOfCards {
     
     public void createDeck() {
